5b718750ab81b9ea2c733bc466b47f4c6add15ba
Ivan Hu <ivanhu2246@gmail.com>
2021-07-21 21:14:25 +0800

    Completed up to 3.6

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/info', (request, response) => {
+  const responseString = `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+  response.send(responseString)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  console.log('POST request body: ', request.body)
+
+  // Check if name already exists in the phonebook
+  if (persons.find(element => element.name === request.body.name)) {
+    response.json( { error: 'name must be unique' } )
+  }
+
+  // Valid request iff request.body.name && request.body.number && two attributes
+  if (!(request.body.hasOwnProperty('name') && request.body.hasOwnProperty('name') && Object.keys(request.body).length === 2)) {
+    response.status(400).end()
+  }
+
+  let newPerson = request.body
+  newPerson.id = Math.floor(Math.random() * 1000)
+  console.log('id generated for newPerson: ', newPerson.id)
+  while (persons.find(element => element.id === newPerson.id)) {
+    newPerson.id = Math.floor(Math.random() * 1000)
+    console.log('id generated for newPerson: ', newPerson.id)
+  }
+  console.log(newPerson)
+  persons.push(newPerson)
+  response.json(newPerson)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
